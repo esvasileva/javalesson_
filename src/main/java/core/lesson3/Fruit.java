0b264f7f6a329c5879221a5182243c84721a05b8
esvasileva <e.s.vasileva@internet.ru>
2022-03-29 13:40:00 +0300

    added task for lesson3

---
+++ src/main/java/core/lesson3/Fruit.java
@@ -0,0 +1,17 @@
+package core.lesson3;
+
+public class Fruit {
+    private float weigh;
+
+    public Fruit(float weigh) {
+        this.weigh = weigh;
+    }
+
+    public float getWeigh() {
+        return weigh;
+    }
+
+    public void setWeigh(float weigh) {
+        this.weigh = weigh;
+    }
+}
---
+++ src/main/java/core/lesson3/Orange.java
@@ -0,0 +1,12 @@
+package core.lesson3;
+
+public class Orange extends Fruit{
+    public Orange (float weigh){
+        super(1.5f);
+    }
+
+    @Override
+    public String toString() {
+        return "Orange";
+    }
+}
